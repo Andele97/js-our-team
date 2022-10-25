4b706b437a800b267673199f7e27bc59f6a8d222
Andele97 <108498619+Andele97@users.noreply.github.com>
2022-10-25 14:04:08 +0200

    [add] dati

--- js/script.js
+++ js/script.js
@@ -2,5 +2,37 @@ const team = [
   {
     nome: "Wayne",
     cognome: "Barnet",
-    professione: "Founder & CEO"
-}]
+    professione: "Founder & CEO",
+    immagine: "wayne-barnett-founder-ceo.jpg"
+},
+  {
+    nome: "Angela",
+    cognome: "Caroll",
+    professione: "Chief Editor",
+    immagine: "angela-caroll-chief-editor.jpg"
+},
+  {
+    nome: "Walter",
+    cognome: "Gordon",
+    professione: "Office Manager",
+    immagine: "walter-gordon-office-manager.jpg"
+},
+  {
+    nome: "Angela",
+    cognome: "Lopex",
+    professione: "Social Media Manager",
+    immagine: "angela-lopez-social-media-manager.jpg"
+},
+  {
+    nome: "Scott",
+    cognome: "Estrada",
+    professione: "Developer",
+    immagine: "scott-estrada-developer.jpg"
+},
+  {
+    nome: "Barbara",
+    cognome: "Ramos",
+    professione: "Graphic Designer",
+    immagine: "barbara-ramos-graphic-designer.jpg"
+}
+]
